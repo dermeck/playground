efcbafa62182d32989d67dd0caffb923446f73f1
dermeck <dermeck@posteo.de>
2023-01-07 14:08:45 +0100

    ws-chatapp: display messages

--- ws-chat/client/chat-client-vue/src/App.vue
+++ ws-chat/client/chat-client-vue/src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
 import {io} from 'socket.io-client';
+import { onBeforeMount, ref } from 'vue';
 
 const socket = io('http://localhost:3001');
+
+const messages = ref([]);
+
+onBeforeMount(() => {
+  socket.emit('findAllMessages', {}, response => {
+    messages.value = response;
+  })
+})
 
 </script>
 
 <template>
   <div class="chat">
-
+    <div class="chat-container">
+      <div class="message-container">
+        <div v-for="message in messages">
+          [{{ message.userName }}]: {{ message.content }}
+        </div>
+      </div>
+    </div>
 
   </div>
 
